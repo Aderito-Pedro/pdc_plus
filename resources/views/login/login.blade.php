158e2f9c1ac3b9ae72cc26ce5b92ca6e67bbbd1a
Aderito-Pedro <aderitopedro95@gmail.com>
2021-06-13 21:31:21 +0100

    tela de login

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt" >
+<head>
+  <meta charset="UTF-8">
+  <title>PDC plus</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="shortcut icon" href="img/logo/favicon.png">
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+  <div class="box">
+  </div>
+  <div class="container-forms">
+    <div class="container-info">
+      <div class="info-item">
+        <div class="table">
+          <div class="table-cell">
+            <p>
+                Sé tens conta?
+            </p>
+            <div class="btn">
+              conecta-se
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="info-item">
+        <div class="table">
+          <div class="table-cell">
+            <p>
+                Não tem uma conta?
+            </p>
+            <div class="btn">
+              Inscrever-se
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-form"> 
+            <img src="img/logo/f38bd0b32f844da9b9f4ff23f185dede.png" alt="Login Background" class="logo">
+      <div class="form-item log-in">
+        <div class="table">
+          <div class="table-cell">
+            <input name="Username" placeholder="Nome do Utilizador ou Email" type="text" />
+            <input name="Password" placeholder="Senha" type="Password" />
+            <div class="btn">
+              conecta-se
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="form-item sign-up">
+        <div class="table">
+          <div class="table-cell">
+            <input name="email" placeholder="Email" type="text" />
+            <input name="fullName" placeholder="Nome Completo" type="text" />
+            <input name="Username" placeholder="Nome do Utilizador" type="text" />
+            <input name="Password" placeholder="Senha" type="Password" />
+            <div class="btn">
+              Inscrever-se
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- partial -->
+  <script src='js/jquery-3.4.1.js'></script>
+  <script  src="js/login.js"></script>
+
+</body>
+</html>
